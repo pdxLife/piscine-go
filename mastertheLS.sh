f8a30568a0e97ced2df2c6a5633ce3b226d82dd5
pdxLife <student@ALEM-7dbc57.alem.school>
2019-10-14 19:03:43 +0600

    LS

--- mastertheLS.sh
+++ mastertheLS.sh
@@ -1,6 +1,6 @@
 #ls -Atr | tr '\n' ','
 
-ls -lcrt --format=comma -p | tr -d ' '
+#ls -lcrt --format=comma -p | tr -d ' '
 
 
-#ls -c -m --indicator-style=comma | tr -d ' '
+ls -c -m --indicator-style=comma | tr -d ' '
