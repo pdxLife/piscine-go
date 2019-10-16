eef8123c3c7db59fe3cd3d5953ac121b15dfaac7
pdxLife <student@ALEM-7dbc57.alem.school>
2019-10-16 17:20:48 +0600

    commiting good one

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,10 +3,10 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	i := 122
-	for i < 98 {
-		z01.PrintRune(rune(i))
-		i++
+	var i rune = 'z'
+	for i >= 'a' {
+		z01.PrintRune(i)
+		i--
 	}
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 }
