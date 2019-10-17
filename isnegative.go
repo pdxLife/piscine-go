7f77af9cc062f1c5c7aba7b4875944c52ffdca9c
pdxLife <student@ALEM-7dbc57.alem.school>
2019-10-17 16:15:14 +0600

    IsNegative

---
+++ isnegative.go
@@ -0,0 +1,13 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func IsNegative (i int) {
+//	var i int
+	if i < 0 {
+			z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
+	}
+z01.PrintRune(10)
+}
