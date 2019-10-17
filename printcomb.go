5637ce84dab653aeb794a3fb28e006a5e2a5a270
pdxLife <student@ALEM-7dbc57.alem.school>
2019-10-17 17:42:50 +0600

    PrintComb v3

--- printcomb.go
+++ printcomb.go
@@ -7,19 +7,15 @@ import "github.com/01-edu/z01"
 func PrintComb () {
 	for a := '0' ; a <= '9'; a++ {
 		for b := '0'; b <= '9'; b++ {
-			for c :='0'; c <= '9'; c++ {
+			for c := '0'; c <= '9'; c++ {
 				if a < b && b < c {
-						z01.PrintRune(a)
-						z01.PrintRune(b)
-						z01.PrintRune(c)
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
-					}
-				
-				}				
-		} 
-			
+					z01.PrintRune(a)
+					z01.PrintRune(b)
+					z01.PrintRune(c)
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}	
+			}				
+		} 		
 	} 	
-	
 }
-
