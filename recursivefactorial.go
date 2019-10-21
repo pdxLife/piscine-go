ef95e0d8d2afc81acbd41589d3a8a2380f62731a
pdxLife <student@ALEM-a1d87c.alem.school>
2019-10-21 19:21:09 +0600

    Recursive

---
+++ recursivefactorial.go
@@ -0,0 +1,37 @@
+package piscine
+
+var arg int
+
+func RecursiveFactorial(nb int) int {
+
+	if nb >= 0 && nb < 20 {
+
+		if nb == 1 {
+			return 1
+		} else if nb > 1 {
+			return nb * RecursiveFactorial(nb-1)
+		} else {
+			return 1
+		}
+	}
+	return 0
+}
+
+// package piscine
+
+// var arg int
+
+// func IterativeFactorial(nb int) int {
+
+// 	if nb >= 0 && nb < 25 {
+
+// 		result := 1
+
+// 		for i := 1; i <= nb; i++ {
+// 			result = result * i
+// 		}
+// 		return result
+// 	} else {
+// 		return 0
+// 	}
+// }
