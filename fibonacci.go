c706a0042c45f4d8fb83c5c58cbaa9d9924df781
pdxLife <student@ALEM-a1d87c.alem.school>
2019-10-21 21:25:32 +0600

    fibonacci v3

--- fibonacci.go
+++ fibonacci.go
@@ -9,9 +9,13 @@ func Fibonacci(index int) int {
 	// }
 	// return result
 
-	if index <= 1 {
+	if index < 0 {
 		return -1
-	}
+	} else if index == 0 {
+		return 0
+	} else if index == 1 {
+		return 1
+	} else
 	return Fibonacci(index-1) + Fibonacci(index-2)
 
 }
