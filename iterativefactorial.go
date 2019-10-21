0561476b092c1579d052974d7717ad4565d62b7e
pdxLife <student@ALEM-a1d87c.alem.school>
2019-10-21 18:49:21 +0600

    iterative v4

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,11 +4,11 @@ var arg int
 
 func IterativeFactorial(nb int) int {
 
-	if nb > 0 && nb < 25 {
+	if nb >= 0 && nb < 25 {
 
 		result := 1
 
-		for i := 2; i <= nb; i++ {
+		for i := 1; i <= nb; i++ {
 			result = result * i
 		}
 		return result
