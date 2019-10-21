7387a9b6ba097150354491f8ecaf8b52a68595e2
pdxLife <student@ALEM-a1d87c.alem.school>
2019-10-21 21:28:43 +0600

    fibonacci v4

--- fibonacci.go
+++ fibonacci.go
@@ -15,7 +15,7 @@ func Fibonacci(index int) int {
 		return 0
 	} else if index == 1 {
 		return 1
-	} else
-	return Fibonacci(index-1) + Fibonacci(index-2)
-
+	} else {
+		return Fibonacci(index-1) + Fibonacci(index-2)
+	}
 }
