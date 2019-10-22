b9cd96e18c0e97d2740f9536081a30e773e43c81
pdxLife <44197020+pdxLife@users.noreply.github.com>
2019-10-22 23:51:32 +0600

    to upper v3

--- toupper.go
+++ toupper.go
@@ -2,10 +2,10 @@ package piscine
 
 func ToUpper(s string) string {
 	word := []byte(s)
-	for index, letter := range wordByte {
+	for index, letter := range word {
 		if letter <= 122 && letter >= 97 {
 			word[index] = letter - 32
 		}
 	}
-	return string(wordByte)
+	return string(word)
 }
