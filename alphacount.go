fa272b1a83c6b81c910152cbf26907ab71f228cc
pdxLife <student@ALEM-317041.alem.school>
2019-10-22 15:34:18 +0600

    alphacount

---
+++ alphacount.go
@@ -0,0 +1,22 @@
+package piscine
+
+import "fmt"
+
+func AlphaCount(str string) int {
+
+	counter := 0
+
+	// for index, letter := range str {
+
+	for _, letter := range str {
+
+		if (letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z') {
+			counter++
+
+			//fmt.Printf("index is %v     letter is &w \n", index, letter)
+		}
+	}
+	fmt.Printf("Counter value is %v \n", counter)
+	//fmt.Println()
+	return counter
+}
