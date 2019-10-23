1a093dab9942b20b5aa9eb073e02fef92bdc1715
pdxLife <student@ALEM-a1d8a1.alem.school>
2019-10-23 19:36:41 +0600

    print nbr order v6

--- Quest05/02.printnbrinorder/printnbrinorder/printnbrinorder.go
+++ Quest05/02.printnbrinorder/printnbrinorder/printnbrinorder.go
@@ -1,15 +1,53 @@
 package piscine
 
+import "github.com/01-edu/z01"
+
+func IntToDigit(n int) (digits []int) {
+
+	for n > 0 {
+		if n == 0 {
+			digits = append(digits, n)
+		} else {
+			digits = append(digits, n%10)
+		}
+		n = n / 10
+	}
+	return
+
+}
+
+func sort(array []int) []int {
+
+	counter := 0
+	for range array {
+
+		counter = counter + 1
+	}
+	for i := counter; i > 0; i-- {
+		for j := 1; j < 1; j++ {
+
+			if array[j] < array[j+1] {
+				value := array[j]
+				array[j] = array[j-1]
+				array[j-1] = value
+			}
+		}
+	}
+	return array
+}
+
 func PrintNbrInOrder(n int) {
 
-	z := []int{n}
+	if n == 0 {
 
-	// counter := 0
+		z01.PrintRune('0')
+		return
+	}
 
-	// x := string(n)
+	for _, c := range sort(IntToDigit(n)) {
 
-	// for i:=0; i
-	//return z
+		z01.PrintRune(rune(c) + '0')
+	}
 
 }
 
