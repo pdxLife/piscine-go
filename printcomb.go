9ee54bb09851f1f945683113a5db7778ac338e98
pdxLife <student@ALEM-a1d873.alem.school>
2019-10-24 17:24:39 +0600

    print comb v3

--- printcomb.go
+++ printcomb.go
@@ -1,24 +1,29 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
-//var a, b, c int
+import (
+	"github.com/01-edu/z01"
+)
 
 func PrintComb() {
-	for a := '0'; a <= '9'; a++ {
-		for b := '0'; b <= '9'; b++ {
-			for c := '0'; c <= '9'; c++ {
-				if a < b && b < c {
-					z01.PrintRune(a)
-					z01.PrintRune(b)
-					z01.PrintRune(c)
-					if a <= '6' {
+
+	for i := '0'; i < '7'; i++ {
+		for j := '0'; j <= '8'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				if i < j {
+					if j < k {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(k)
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
 				}
 			}
+
 		}
 	}
+	z01.PrintRune('7')
+	z01.PrintRune('8')
+	z01.PrintRune('9')
 	z01.PrintRune('\n')
 }
