db2d4c9639bd64691bf42b5e5e4d60afeb0f31aa
pdxLife <student@ALEM-a1d873.alem.school>
2019-10-24 16:23:01 +0600

    Quest 05

---
+++ Quest05/04.firstrune/firstrune/firstrune.go
@@ -0,0 +1,5 @@
+package piscine
+
+func FirstRune(s string) rune {
+
+}
---
+++ Quest05/04.firstrune/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	piscine ".."
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	z01.PrintRune(piscine.FirstRune("Hello!"))
+	z01.PrintRune(piscine.FirstRune("Salut!"))
+	z01.PrintRune(piscine.FirstRune("Ola!"))
+	z01.PrintRune('\n')
+}
