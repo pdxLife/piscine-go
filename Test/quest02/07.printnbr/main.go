27f04f1acd78d5576b03e7f758f453866142e6d1
pdxLife <azabek87@gmail.com>
2019-10-26 23:27:38 +0600

    updating parent folder

---
+++ Test/quest02/07.printnbr/main.go
@@ -0,0 +1,9 @@
+package main
+
+import piscine "./printnbr"
+
+func main() {
+	piscine.PrintNbr(-123)
+	piscine.PrintNbr(0)
+	piscine.PrintNbr(123)
+}
---
+++ Test/quest02/07.printnbr/printnbr/printnbr.go
@@ -0,0 +1,10 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+
+	if n 
+	for i 
+
+}
